ec0e225775d40f8d21f8dec4a32ec4545e62a13a
LabnicUofM <77589923+LabnicUofM@users.noreply.github.com>
2023-04-02 18:41:54 -0400

    fixed better better better better

--- src/OrderBeverageService/OrderBeverageHandler.h
+++ src/OrderBeverageService/OrderBeverageHandler.h
@@ -83,7 +83,7 @@ namespace vending_machine{
       //3. get beverage name
       std::string beverageName ="";
       try {
-        beverageName = getbeverage_client->ReturnBeverage((int)weatherType);
+        getbeverage_client->ReturnBeverage(beverageName,(int)weatherType);
       } 
       catch (...) {
         _getbeverage_client_pool->Push(getbeverage_client_wrapper);
