fdb63fe6534a35bd10d64fbdc049083e9a8203e3
Labnic <Labnic@node-0.labnic-chal-2.umich-d-cs-edu-pg0.utah.cloudlab.us>
2023-04-02 18:55:24 -0600

    asfsdnjkn

---
+++ DockerRun.sh
@@ -0,0 +1,4 @@
+#1. Run docker-compose script to run all the containers:
+sudo docker-compose up
+#2. Stop docker containers that started from docker-compose script:
+sudo docker-compose down
---
+++ ThriftBuild.sh
@@ -0,0 +1,5 @@
+thrift --gen cpp vending_machine.thrift
+
+thrift --gen py vending_machine.thrift
+
+thrift --gen lua vending_machine.thrift
