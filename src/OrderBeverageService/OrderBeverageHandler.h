565f697e78fccb1e53123f6f58d7e6dafd539fbf
LabnicUofM <77589923+LabnicUofM@users.noreply.github.com>
2023-04-02 20:06:36 -0400

    Update OrderBeverageHandler.h

--- src/OrderBeverageService/OrderBeverageHandler.h
+++ src/OrderBeverageService/OrderBeverageHandler.h
@@ -46,55 +46,55 @@ namespace vending_machine{
       _return = "after anything";
       //return BeverageType::type::COLD;
    
-      // 1. get the weather service client pool
-      auto weather_client_wrapper = _weather_client_pool->Pop();
-      if (!weather_client_wrapper) {
-        ServiceException se;
-        se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
-        se.message = "Failed to connect to weather-service";
-        throw se;
-      }
-      auto weather_client = weather_client_wrapper->GetClient();
+      // // 1. get the weather service client pool
+      // auto weather_client_wrapper = _weather_client_pool->Pop();
+      // if (!weather_client_wrapper) {
+      //   ServiceException se;
+      //   se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
+      //   se.message = "Failed to connect to weather-service";
+      //   throw se;
+      // }
+      // auto weather_client = weather_client_wrapper->GetClient();
 
-      // by default get cold
-      WeatherType::type weatherType = WeatherType::type::COLD;
+      // // by default get cold
+      // WeatherType::type weatherType = WeatherType::type::COLD;
 
-      // 2. call the remote procedure : GetWeather
-      try {
-        weatherType = weather_client->GetWeather(city);
-      } 
-      catch (...) {
-        _weather_client_pool->Push(weather_client_wrapper);
-        LOG(error) << "Failed to send call GetWeather to weather-client";
-        throw;
-      }
-      int64_t intWeatherType = weatherType == WeatherType::type::COLD ? 1 : 0;
-      _weather_client_pool->Push(weather_client_wrapper);
-      _return = "after weather";
+      // // 2. call the remote procedure : GetWeather
+      // try {
+      //   weatherType = weather_client->GetWeather(city);
+      // } 
+      // catch (...) {
+      //   _weather_client_pool->Push(weather_client_wrapper);
+      //   LOG(error) << "Failed to send call GetWeather to weather-client";
+      //   throw;
+      // }
+      // int64_t intWeatherType = weatherType == WeatherType::type::COLD ? 1 : 0;
+      // _weather_client_pool->Push(weather_client_wrapper);
+      // _return = "after weather";
 
-      auto getbeverage_client_wrapper = _getbeverage_client_pool->Pop();
-      if (!getbeverage_client_wrapper) {
-        ServiceException se;
-        se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
-        se.message = "Failed to connect to getbeverage-service";
-        throw se;
-      }
-      auto getbeverage_client = getbeverage_client_wrapper->GetClient();
+      // auto getbeverage_client_wrapper = _getbeverage_client_pool->Pop();
+      // if (!getbeverage_client_wrapper) {
+      //   ServiceException se;
+      //   se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
+      //   se.message = "Failed to connect to getbeverage-service";
+      //   throw se;
+      // }
+      // auto getbeverage_client = getbeverage_client_wrapper->GetClient();
 
-      //3. get beverage name
-      std::string beverageName ="test";
-      try {
-        //getbeverage_client->ReturnBeverage(*beverageName,intWeatherType);
-      } 
-      catch (...) {
-        _getbeverage_client_pool->Push(getbeverage_client_wrapper);
-        LOG(error) << "Failed to send call ReturnBeverage to getbeverage-client";
-        throw;
-      }
-      _getbeverage_client_pool->Push(getbeverage_client_wrapper);    
+      // //3. get beverage name
+      // std::string beverageName ="test";
+      // try {
+      //   //getbeverage_client->ReturnBeverage(*beverageName,intWeatherType);
+      // } 
+      // catch (...) {
+      //   _getbeverage_client_pool->Push(getbeverage_client_wrapper);
+      //   LOG(error) << "Failed to send call ReturnBeverage to getbeverage-client";
+      //   throw;
+      // }
+      // _getbeverage_client_pool->Push(getbeverage_client_wrapper);    
       
 
-      _return = beverageName;
+      // _return = beverageName;
   }
 
 } // namespace vending_machine
